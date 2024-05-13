02481b5ad443c695f3be71ac49e3296205627654
HOSEWELL <hosewellkaranja@gmail.com>
2024-05-13 14:11:10 +0300

    first commit

---
+++ java.js
@@ -0,0 +1,40 @@
+{
+    const pi=3.142;
+    console.log(pi);
+    let d=21;
+    console.log(d);
+    let r = d/2;
+    console.log(r);
+    let a = pi*r**2
+}
+{
+    let h = 8;
+    console.log(h);
+    h*=10;
+    h+=5;
+    console.log(h);
+}
+{
+    let h = 5;
+    console
+}
+{
+    let a = 3;
+    let b = a++;
+    console.log(b);
+
+}
+{
+    let a = 6;
+let b = ++a;
+console.log(a);
+console.log(b);
+
+}
+{
+
+    let = 16;
+    let
+}
+
+
